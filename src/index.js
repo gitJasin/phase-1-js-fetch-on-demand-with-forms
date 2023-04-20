443d3f27610eb9037976170781523094b9f25127
Jason Stewart <jaysin.stewart@gmail.com>
2023-04-20 11:02:19 -0700

    added form and submit event for form

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form')
+
+  inputForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log(e)
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
